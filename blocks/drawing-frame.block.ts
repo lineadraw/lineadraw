cef4ab41589bf7d08c25d9b5459f3fff3dabc7b8
Mikhail Koshelev <koshelev.mikhail@outlook.com>
2026-08-27 15:21:46 +0300

    feat: update drawing frame block version to 1.2.0 and add customizable margin and visibility parameters

--- blocks/drawing-frame.block.ts
+++ blocks/drawing-frame.block.ts
@@ -6,7 +6,7 @@ export default defineBlock({
   id: "@lineadraw/frame",
   name: "Drawing frame",
   description: "Draws a drawing frame.",
-  version: "1.1.1",
+  version: "1.2.0",
   authors: ["Linea Team"],
   tags: ["annotation", "drawing"],
   params: [
@@ -59,6 +59,24 @@ export default defineBlock({
       default: 1,
       min: 1,
     },
+    {
+      name: "m",
+      label: "Margin",
+      type: "number",
+      default: 5,
+    },
+    {
+      name: "showSeparators",
+      label: "Show ticks",
+      type: "boolean",
+      default: true,
+    },
+    {
+      name: "showOuter",
+      label: "Show outer",
+      type: "boolean",
+      default: false,
+    },
   ],
   paramVisibility: ({ params }) => {
     const { size } = params;
@@ -68,13 +86,25 @@ export default defineBlock({
       height: size === "Custom",
       x: size === "xA4",
       y: size === "xA4",
+      showSeparators: size === "xA4",
     };
   },
   draw: ({ params }) => {
-    const { size, orientation, width, height, x, y } = params;
+    const {
+      size,
+      orientation,
+      width,
+      height,
+      x,
+      y,
+      m,
+      showSeparators,
+      showOuter,
+    } = params;
 
     let w = 20;
     let h = 20;
+    const separators: Line[] = [];
 
     if (size === "Custom") {
       w = width;
@@ -82,6 +112,21 @@ export default defineBlock({
     } else if (size === "xA4") {
       w = 210 * x;
       h = 297 * y;
+
+      for (let i = 1; i < x; i++) {
+        const x = i * 210;
+        separators.push(
+          { type: "line", a: [x, 0], b: [x, m] },
+          { type: "line", a: [x, h], b: [x, h - m] },
+        );
+      }
+      for (let i = 1; i < y; i++) {
+        const y = i * 297;
+        separators.push(
+          { type: "line", a: [0, y], b: [m, y] },
+          { type: "line", a: [w, y], b: [w - m, y] },
+        );
+      }
     } else if (paperSizes[size]) {
       const paper = paperSizes[size];
       w = orientation === "Portrait" ? paper.w : paper.h;
@@ -89,22 +134,37 @@ export default defineBlock({
     }
 
     return [
+      ...(showOuter
+        ? [
+            {
+              type: "polyline",
+              points: [
+                [0, 0],
+                [w, 0],
+                [w, h],
+                [0, h],
+              ],
+              closed: true,
+            } as Polyline,
+          ]
+        : []),
       {
         type: "polyline",
         points: [
-          [5, 5],
-          [w - 5, 5],
-          [w - 5, h - 5],
-          [5, h - 5],
+          [m, m],
+          [w - m, m],
+          [w - m, h - m],
+          [m, h - m],
         ],
         closed: true,
       },
+      ...(showSeparators ? separators : []),
     ];
   },
 });
 
 const paperSizes: Record<string, { w: number; h: number }> = {
-  A1: { w: 594, h: 891 },
+  A1: { w: 594, h: 841 },
   A2: { w: 420, h: 594 },
   A3: { w: 297, h: 420 },
   A4: { w: 210, h: 297 },
